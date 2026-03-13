fe5c3dcb7181d565bbd0598f916e2ae39d74f784
Hyeoninii <hyeoninii0314@gmail.com>
2026-03-13 17:33:14 +0900

    modify:code block style

--- _javascript/modules/components.js
+++ _javascript/modules/components.js
@@ -5,6 +5,5 @@ export { imgPopup } from './components/img-popup';
 export { initLocaleDatetime } from './components/locale-datetime';
 export { initToc } from './components/toc';
 export { loadMermaid } from './components/mermaid';
-export { modeWatcher } from './components/mode-toggle';
 export { back2top } from './components/back-to-top';
 export { loadTooptip } from './components/tooltip-loader';
